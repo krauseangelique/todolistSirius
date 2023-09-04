10e15540dbba37e1d75ba1e47dc110ad37af0857
krauseangelique <krause_angelique@hotmail.com>
2023-09-04 11:36:43 +0200

    add prevent default

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
+console.log("Hello");
 const form = document.getElementsByTagName("form")[0];
-const output = document.getElementsByTagName("ul")[0];
 
+const input = document.querySelector("input[type=text]");
+
+const output = document.getElementsByTagName("ul")[0];
+console.log(output);
+
+/* Add Task */
 function addTask(e) {
+
+  // Don't refresh the page!
   e.preventDefault();
   const taskValue = form[0].value;
 
   if(taskValue === ""){
     return null;
+  } else {
+    console.log(taskValue);
+
+    output.innerHTML += `<li>
+    <p>${taskValue}</p>
+    <div>
+      <button class="done"></button>
+      <button class="delete">
+        Delete
+      </button>
+    </div>
+  </li>`;
+
+  // Before adding a new task in the input we must remove the old one
+  form[0].value = '';
+
   }
-  console.log(taskValue);
-
-  output.innerHTML += `<li>
-  <p>${taskValue}</p>
-  <div>
-    <button class="done"></button>
-    <button class="delete">
-      Delete
-    </button>
-  </div>
-</li>`;
 }
-addTask();
-console.log(taskValue);
+//addTask(); // Error
+// console.log(taskValue); // Error
 
+// Event on the FORM
 form.addEventListener("submit", addTask);
 /**
  *  stages of the project :
@@ -36,4 +50,22 @@ Enregistrer toutes les tâches dans votre navigateur !
  */
 // some informations credited by:
 /* https://fr.moonbooks.org/Articles/Comment-ajouter-un-nouveau-li-a-ul-en-utilisant-javascript-/#:~:text=Ajouter%20li%20%C3%A0%20ul%20en%20utilisant%20javascript%20Pour,text%20%3D%20document.createTextNode%20%28%27item%204%20%28added%20using%20javascript%29%27%29%3B */
+
+// choose the add ok button DONE
+let recherche = document.querySelectorAll("button[class=done]");
+console.log(recherche);
+
+for (let index = 0; index < recherche.length; index++) {
+    const element = recherche[index];
+    
+
+    recherche[index] = addEventListener("click", () => {
+        element.removeAttribute("done");
+        element.focus();
+        element.innerText = "Undo";
+        console.log("élément CLICKÉ");
+
+    });
+    
+}
 
